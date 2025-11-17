d496cb521feda2bc5074411b1a6896fd46847702
RondneyLoiola <rondney.loiola11@gmail.com>
2025-11-16 23:42:45 -0300

    schema de order

---
+++ src/app/schemas/Order.js
@@ -0,0 +1,55 @@
+//mongo trabalha com schemas e não com migrations
+
+import mongoose from 'mongoose';
+
+const OrderSchema = new mongoose.Schema(
+  {
+    user: {
+      id: {
+        type: String,
+        required: true,
+      },
+      name: {
+        type: String,
+        required: true,
+      },
+    },
+    products: [
+      {
+        id: {
+          type: Number,
+          required: true,
+        },
+        name: {
+          type: String,
+          required: true,
+        },
+        price: {
+          type: String,
+          required: true,
+        },
+        category: {
+          type: String,
+          required: true,
+        },
+        quantity: {
+          type: Number,
+          required: true,
+        },
+        url: {
+          type: String,
+          required: true,
+        },
+      },
+    ],
+    status: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true, //faz o createdAt e updatedAt automaticamente
+  },
+);
+
+export default mongoose.model('Order', OrderSchema);
