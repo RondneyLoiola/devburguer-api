77047b4277beda58e9e398c4c0ebeffc9fec4ac7
RondneyLoiola <rondney.loiola11@gmail.com>
2025-11-17 14:14:41 -0300

    ajuste no docker e cors

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
+import cors from 'cors';
 import express from 'express';
 import fileRouteConfig from './config/fileRoutes.cjs';
 import routes from './routes.js';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })); //por causa do type module
 app.use('/product-file', fileRouteConfig); //rota para ver a foto do produto
